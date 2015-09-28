3bf18c33b5c00c0c76bad0b406e6efffc9e887dd
Eric <eric-pierce@west-mec.org>
2015-09-28 15:19:23 -0700

    I don't know what this does

--- Game.js
+++ Game.js
@@ -20,13 +20,13 @@ while (slaying) {
             youHit = Math.floor(Math.random() * 2);
         }
     } else {
-        alert("The Dragon burns you to a crisp. No one can identify you. You LOSE!");
+        alert("The Dragon burns you to a crisp. No one can identify your burnt corpse =( . You LOSE!");
         slaying = false;
     }
 }
 game();
-/* this is going to play the game again (refresh or re-do i don't know) then for the else if it will make the tab close
- (still need the code to put it in)
+/* this is going to play the game again
+and else if is doing nothing.
  */
 function game() {
     var again = prompt("Do you want to play again? (Y/N)")
